e5c4086f1e4eca4f74c0c98d7cc6dffee2ba3e95
Prince Nmezi <stephenprince427@gmail.com>
2023-04-30 01:13:25 +0100

    updated api section

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCd16jUyQwzwQJwnxMcyRKnCrEWqpzEthM",
-    authDomain: "netflix-clone-2afa2.firebaseapp.com",
-    projectId: "netflix-clone-2afa2",
-    storageBucket: "netflix-clone-2afa2.appspot.com",
-    messagingSenderId: "84829324054",
-    appId: "1:84829324054:web:efd55fb834f1e9a9cb4637"
+
 };
   
 const firebaseApp = firebase.initializeApp(firebaseConfig);
